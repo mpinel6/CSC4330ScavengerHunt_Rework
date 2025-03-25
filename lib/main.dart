db2d347a97dc66fbef4abe63bc06fd3f12b23e17
IanWask <iwasko1@lsu.edu>
2025-03-24 23:02:51 -0500

    Removed redundant code due to new game_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'game_screen.dart'; // Import the separate game screen file
 
 void main() {
   runApp(const MyApp());
@@ -34,6 +35,7 @@ class _MainScreenState extends State<MainScreen> {
   static const List<Widget> _screens = [
     MapScreen(),
     LocationsScreen(),
+    // Moved the code for the GamesScreen to a separate file.
     GamesScreen(),
     HelpScreen(),
   ];
@@ -603,48 +605,7 @@ class _LocationsScreenState extends State<LocationsScreen> {
   }
 }
 
-class GamesScreen extends StatelessWidget {
-  const GamesScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFA39AAC),
-      appBar: AppBar(
-        title: const Text(
-          'Games',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Color(0xFFFDD023),
-            shadows: [
-              Shadow(
-                offset: Offset(1.0, 1.0),
-                blurRadius: 3.0,
-                color: Color(0xFF000000),
-              ),
-            ],
-          ),
-        ),
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF5A2B8C), // Lighter purple for top
-                Color(0xFF461D7C), // Main purple for middle
-                Color(0xFF3A1B6C), // Darker purple for bottom
-              ],
-            ),
-          ),
-        ),
-      ),
-      body: const Center(
-        child: Text('Games Screen'),
-      ),
-    );
-  }
-}
+// The GamesScreen code was moved to game_screen.dart
 
 class HelpScreen extends StatelessWidget {
   const HelpScreen({super.key});
